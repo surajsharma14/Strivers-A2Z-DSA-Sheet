afcefb47d12e2aa3c15084bc25207f08977866ef
Suraj Kumar Sharma <72158521+surajsharma14@users.noreply.github.com>
2023-07-10 22:49:55 +0530

    Create Palindrome Number.cpp

---
+++ Know Basic Math/Palindrome Number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int revNum = 0;
+    int dup = n;
+    while(n>0) {
+        int ld = n% 10;
+        revNum = (revNum * 10) +  ld;
+        n = n / 10;
+    }
+    if(dup == revNum) cout << "true";
+    else cout << "false";
+}
