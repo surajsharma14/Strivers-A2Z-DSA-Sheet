5177e5f33763e4ab47d4dcb9be646edf8aee5f9f
Suraj Kumar Sharma <72158521+surajsharma14@users.noreply.github.com>
2023-08-23 15:24:48 +0530

    Create  Displaying Array Elements.cpp

---
+++ Array/Displaying Array Elements.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {19, 10, 8, 17, 9};
+    for(int i =0; i < 5;++i) {
+      cout<< marks[i] << " ";  
+    }
+    
+    
+    return 0;
+}
